21bf240365603903ec89e4de5fcecd250bfe4e3e
lgr0113@126.com <l5854531>
2020-06-10 17:43:40 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,21 @@
 //Page Object
 Page({
   data: {
-    
+    //轮播图数组
+    swiperList:[]
   },
   //options(Object)
+  //页面开始加载 就会触发事件
   onLoad: function(options) {
-    
+    //发送异步请求 获取轮播图
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result)=>{
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    });
   },
   onReady: function() {
     
